b1863a9ce6f8441a04b2ba8646ce7970a0655b36
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-05-26 19:17:18 +0700

    fix bug shirt

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { Center, Environment } from "@react-three/drei";
+import { Environment, Center } from "@react-three/drei";
 import Shirt from "./Shirt";
 import Backdrop from "./Backdrop";
 import CameraRig from "./CameraRig";
@@ -10,16 +10,16 @@ const CanvasModel = () => {
       shadows
       camera={{ position: [0, 0, 0], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
-      className="w-full max-w-full transition-all ease-in"
+      className="w-full max-w-full h-full transition-all ease-in"
     >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
+
       <CameraRig>
-        <Backdrop>
-          <Center>
-            <Shirt />
-          </Center>
-        </Backdrop>
+        <Backdrop />
+        <Center>
+          <Shirt />
+        </Center>
       </CameraRig>
     </Canvas>
   );
